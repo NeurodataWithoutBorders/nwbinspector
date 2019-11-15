f4e9f6e8e8da96426b17f92c0b3a0e3ef840883d
Ryan Ly <rly@lbl.gov>
2019-11-15 11:29:56 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A set of tools to help inspect NWB files for compliance with [NWB Best Practices](https://www.nwb.org/best-practices/)
---
+++ pynwb_inspect_dir.py
@@ -0,0 +1,28 @@
+import sys
+from pathlib import Path
+import pynwb
+
+
+def main(dir_name):
+    data_dir = Path(dir_name)
+    if not data_dir.is_dir():
+        raise Exception('%s should be a directory' % dir_name)
+
+    for filename in data_dir.glob('*.nwb'):
+        print(filename)
+
+        try:
+            with pynwb.NWBHDF5IO(str(filename), 'r') as io:
+                pynwb.validate(io)
+                nwbfile = io.read()
+                # inspect NWBFile object
+
+        except Exception as ex:
+            print(ex)
+
+
+if __name__ == '__main__':
+    """
+    Usage: python pynwb_inspect_dir.py dir_name
+    """
+    main(sys.argv[1])
