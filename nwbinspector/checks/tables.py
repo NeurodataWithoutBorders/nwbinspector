de7342670075cee07047539f5fdc79d43c51d1f4
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2022-02-17 15:04:29 -0500

    Update tables.py

--- nwbinspector/checks/tables.py
+++ nwbinspector/checks/tables.py
@@ -16,6 +16,17 @@ def check_empty_table(table: DynamicTable):
 
 @register_check(importance=Importance.BEST_PRACTICE_VIOLATION, neurodata_type=TimeIntervals)
 def check_time_interval_time_columns(time_intervals: TimeIntervals, nelems=200):
+    """
+    Check that time columns are in ascending order.
+
+    Parameters
+    ----------
+    time_intervals: TimeIntervals
+    nelems: int
+        Only check the first {nelems} elements. This is useful in case there columns are
+        very long so you don't need to load the entire array into memory. Use None to
+        load the entire arrays.
+    """
     unsorted_cols = []
     for column in time_intervals.columns:
         if column.name[-5:] == "_time":
