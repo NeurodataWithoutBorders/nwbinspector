bb5f0e79193f468c410fd7c5febee9d4ddb712fc
Cody Baker <cbaker9@nd.edu>
2022-02-17 11:44:40 -0500

    improving auto-color detection

--- nwbinspector/inspector_tools.py
+++ nwbinspector/inspector_tools.py
@@ -9,6 +9,15 @@ from pathlib import Path
 from . import Importance
 from .utils import FilePathType
 
+try:
+    import colorama
+
+    colorama.init()
+except (ImportError, OSError):
+    HAS_COLORAMA = False
+else:
+    HAS_COLORAMA = True
+
 
 def sort_by_descending_severity(check_results: list):
     """Order the dictionaries in the check_list by severity."""
@@ -64,11 +73,38 @@ def supports_color():
 
     From https://github.com/django/django/blob/main/django/core/management/color.py
     """
-    plat = sys.platform
-    supported_platform = plat != "Pocket PC" and (plat != "win32" or "ANSICON" in os.environ)
+
+    def vt_codes_enabled_in_windows_registry():
+        """Check the Windows Registry to see if VT code handling has been enabled by default."""
+        try:
+            # winreg is only available on Windows.
+            import winreg
+        except ImportError:
+            return False
+        else:
+            reg_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Console")
+            try:
+                reg_key_value, _ = winreg.QueryValueEx(reg_key, "VirtualTerminalLevel")
+            except FileNotFoundError:
+                return False
+            else:
+                return reg_key_value == 1
+
     # isatty is not always implemented, #6223.
     is_a_tty = hasattr(sys.stdout, "isatty") and sys.stdout.isatty()
-    return supported_platform and is_a_tty
+
+    return is_a_tty and (
+        sys.platform != "win32"
+        or HAS_COLORAMA
+        or "ANSICON" in os.environ
+        or
+        # Windows Terminal supports VT codes.
+        "WT_SESSION" in os.environ
+        or
+        # Microsoft Visual Studio Code's built-in terminal supports colors.
+        os.environ.get("TERM_PROGRAM") == "vscode"
+        or vt_codes_enabled_in_windows_registry()
+    )
 
 
 def print_to_console(log_file_path: FilePathType, no_color: bool = False):
