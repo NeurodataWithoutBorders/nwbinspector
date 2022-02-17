16dc7f3ff5142f14b86edbdbf40868f9095896d2
bendichter <ben.dichter@gmail.com>
2022-02-17 16:53:24 -0600

    add tests

--- nwbinspector/checks/tables.py
+++ nwbinspector/checks/tables.py
@@ -6,15 +6,16 @@ from pynwb.file import TimeIntervals
 from ..register_checks import register_check, InspectorMessage, Importance
 
 
-@register_check(importance=Importance.BEST_PRACTICE_VIOLATION, neurodata_type=DynamicTable)
+@register_check(importance=Importance.CRITICAL, neurodata_type=DynamicTable)
 def check_dynamic_table_region_data_validity(dynamic_table_region: DynamicTableRegion, nelems=200):
-    if np.any(dynamic_table_region.data[:nelems] > len(dynamic_table_region.table)):
-        return InspectorMessage(message=f"Some elements of {dynamic_table_region.name} are out of range because they "
-                                        f"are greater than the length of the target table. Note that data should "
-                                        f"contain indices, not ids.")
-    if np.any(dynamic_table_region.data[:nelems] < 0):
-        return InspectorMessage(message=f"Some elements of {dynamic_table_region.name} are out of range because they "
-                                        f"are less than 0.")
+    if np.any(np.asarray(dynamic_table_region.data[:nelems]) > len(dynamic_table_region.table)):
+        return InspectorMessage(
+            message=f"Some elements of {dynamic_table_region.name} are out of range because they are greater than the length of the target table. Note that data should contain indices, not ids."
+        )
+    if np.any(np.asarray(dynamic_table_region.data[:nelems]) < 0):
+        return InspectorMessage(
+            message=f"Some elements of {dynamic_table_region.name} are out of range because they are less than 0."
+        )
 
 
 @register_check(importance=Importance.BEST_PRACTICE_VIOLATION, neurodata_type=DynamicTable)
--- tests/unit_tests/test_tables.py
+++ tests/unit_tests/test_tables.py
@@ -1,9 +1,53 @@
+from unittest import TestCase
+
 import pytest
-from hdmf.common import DynamicTable
+from hdmf.common import DynamicTable, DynamicTableRegion
 from pynwb.file import TimeIntervals
 
-from nwbinspector import check_empty_table, check_time_interval_time_columns, check_time_intervals_stop_after_start
+from nwbinspector import (
+    check_empty_table,
+    check_time_interval_time_columns,
+    check_time_intervals_stop_after_start,
+    check_dynamic_table_region_data_validity,
+)
 from nwbinspector.register_checks import InspectorMessage, Importance, Severity
+
+
+class TestCheckDynamicTableRegion(TestCase):
+
+    def setUp(self):
+        self.table = DynamicTable(name="test_table", description="")
+        self.table.add_column(name="test_column", description="")
+        for _ in range(10):
+            self.table.add_row(test_column=1)
+
+    def test_check_dynamic_table_region_data_validity_lt_zero(self):
+        dynamic_table_region = DynamicTableRegion(name="dyn_tab", description="desc", data=[-1, 0], table=self.table)
+        assert check_dynamic_table_region_data_validity(dynamic_table_region) == InspectorMessage(
+            severity=Severity.NO_SEVERITY,
+            message="Some elements of dyn_tab are out of range because they are less than 0.",
+            importance=Importance.CRITICAL,
+            check_function_name="check_dynamic_table_region_data_validity",
+            object_type="DynamicTableRegion",
+            object_name="dyn_tab",
+            location="/",
+        )
+
+    def test_check_dynamic_table_region_data_validity_gt_len(self):
+        dynamic_table_region = DynamicTableRegion(name="dyn_tab", description="desc", data=[0, 20], table=self.table)
+        assert check_dynamic_table_region_data_validity(dynamic_table_region) == InspectorMessage(
+            severity=Severity.NO_SEVERITY,
+            message="Some elements of dyn_tab are out of range because they are greater than the length of the target table. Note that data should contain indices, not ids.",
+            importance=Importance.CRITICAL,
+            check_function_name="check_dynamic_table_region_data_validity",
+            object_type="DynamicTableRegion",
+            object_name="dyn_tab",
+            location="/",
+        )
+
+    def test_pass_check_dynamic_table_region_data(self):
+        dynamic_table_region = DynamicTableRegion(name="dyn_tab", description="desc", data=[0, 0, 2], table=self.table)
+        assert check_dynamic_table_region_data_validity(dynamic_table_region) is None
 
 
 def test_check_empty_table_with_data():
