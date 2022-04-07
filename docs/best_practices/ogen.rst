38967acf1af05c5b5a440c9df086b9162f0bc3d7
bendichter <ben.dichter@gmail.com>
2022-04-07 09:30:55 -0400

    add docstring

--- docs/best_practices/ogen.rst
+++ docs/best_practices/ogen.rst
@@ -7,6 +7,6 @@ OptogeneticSeries
 -----------------
 
 Each :nwb-schema:ref:`sec-OptogeneticStimulusSite` object present in an :nwb-schema:ref:`sec-NWBFile` should
-be referenced by at least on :nwb-schema:ref:`sec-OptogeneticSeries` in the same file.
+be referenced by at least one :nwb-schema:ref:`sec-OptogeneticSeries` in the same file.
 
 Check function: :py:meth:`~nwbinspector.checks.ogen.check_optogenetic_stimulus_site_has_optogenetic_series`
--- nwbinspector/checks/ogen.py
+++ nwbinspector/checks/ogen.py
@@ -5,6 +5,19 @@ from ..register_checks import register_check, Importance, InspectorMessage
 
 @register_check(importance=Importance.BEST_PRACTICE_VIOLATION, neurodata_type=OptogeneticStimulusSite)
 def check_optogenetic_stimulus_site_has_optogenetic_series(ogen_site: OptogeneticStimulusSite):
+    """
+    Ensures that each OptogeneticStimulusSite object present in an NWBFile is referenced by at least one
+    OptogeneticSeries in the same file.
+
+    Parameters
+    ----------
+    ogen_site: OptogeneticStimulusSite
+
+    Returns
+    -------
+    InspectorMessage or None
+
+    """
     nwbfile = ogen_site.get_ancestor("NWBFile")
     for obj in nwbfile.objects.values():
         if isinstance(obj, OptogeneticSeries):
