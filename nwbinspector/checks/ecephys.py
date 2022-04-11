cce0ccd3b2ddfc371787540dc330391e71dd36e1
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2022-04-11 13:52:19 -0400

    Update nwbinspector/checks/ecephys.py
    Co-authored-by: Ben Dichter <ben.dichter@gmail.com>

--- nwbinspector/checks/ecephys.py
+++ nwbinspector/checks/ecephys.py
@@ -58,7 +58,7 @@ def check_spike_times_not_in_unobserved_interval(units_table: Units, nelems: int
     if not units_table.obs_intervals:
         return
     for spike_time in units_table.spike_times.data[:nelems]:
-        if not any((obs_int[0] <= spike_time <= obs_int[1] for obs_int in units_table.obs_intervals.data)):
+        if not any((start <= spike_time <= stop for start, stop in units_table.obs_intervals.data)):
             return InspectorMessage(
                 message=(
                     "This Units table contains spike times that occur during periods of time not labeled as being "
