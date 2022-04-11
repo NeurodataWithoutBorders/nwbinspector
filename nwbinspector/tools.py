103bc1465413331a506b5b3a58ba133a931f6578
Cody Baker <cbaker9@nd.edu>
2022-04-11 14:51:02 -0400

    trying to debug

--- nwbinspector/tools.py
+++ nwbinspector/tools.py
@@ -22,12 +22,14 @@ def all_of_type(nwbfile: NWBFile, neurodata_type):
 
 
 def get_s3_urls_and_dandi_paths(
-    dandiset_id: str, version_id: Optional[str] = None, n_jobs: int = 1, api_url: Optional[str] = None
+    dandiset_id: str, version_id: Optional[str] = None, api_url: Optional[str] = None, n_jobs: int = 1
 ) -> Dict[str, str]:
     """
     Collect S3 URLS from a DANDISet ID.
 
     Returns dictionary that maps each S3 url to the displayed file path on the DANDI archive content page.
+
+    If using the staging server, pass api_url=https://api-staging.dandiarchive.org/api
     """
     assert re.fullmatch(
         pattern="^[0-9]{6}$", string=dandiset_id
@@ -47,13 +49,21 @@ def get_s3_urls_and_dandi_paths(
                 for future in as_completed(futures):
                     s3_urls_to_dandi_paths.update(future.result())
     else:
-        with DandiAPIClient() as client:
+        with DandiAPIClient(api_url=api_url) as client:
             dandiset = client.get_dandiset(dandiset_id=dandiset_id, version_id=version_id)
             for asset in dandiset.get_assets():
-                s3_urls_to_dandi_paths.update({asset.get_content_url(follow_redirects=1, strip_query=True): asset.path})
+                s3_urls_to_dandi_paths.update(_get_content_url_and_path(asset=asset))
     return s3_urls_to_dandi_paths
 
 
-def _get_content_url_and_path(asset, follow_redirects: int = 1, strip_query: bool = True):
+def _get_content_url_and_path(
+    asset, api_url: Optional[str] = None, follow_redirects: int = 1, strip_query: bool = True
+):
     """Private helper function for parallelization in 'get_s3_urls_and_dandi_paths'."""
-    return {asset.get_content_url(follow_redirects=follow_redirects, strip_query=strip_query): asset.path}
+    if api_url:
+        # The DANDIAPIClient.get_dandiset is setting the api_url correctly, but the assets from
+        # DANDIAPIClient.get_dandiset.get_asset do not propagate it
+        asset_url = api_url + "/assets/" + asset.blob + "/download/"
+    else:
+        asset_url = asset.get_content_url(follow_redirects=1, strip_query=True)
+    return {asset_url: asset.path}
