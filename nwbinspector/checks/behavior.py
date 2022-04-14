d9ed8e5340457ef53042beffe1c35c60aa3a810b
Ben Dichter <ben.dichter@gmail.com>
2022-04-14 12:53:29 -0400

    Update nwbinspector/checks/behavior.py

--- nwbinspector/checks/behavior.py
+++ nwbinspector/checks/behavior.py
@@ -6,4 +6,4 @@ from ..register_checks import register_check, Importance, InspectorMessage
 @register_check(importance=Importance.CRITICAL, neurodata_type=SpatialSeries)
 def check_spatial_series_dims(spatial_series: SpatialSeries):
     if len(spatial_series.data.shape) > 1 and spatial_series.data.shape[1] > 3:
-        return InspectorMessage("SpatialSeries should have either 2 columns (x,y) or 3 columns (x,y,z).")
+        return InspectorMessage("SpatialSeries should have 1 column (x, ), 2 columns (x, y), or 3 columns (x, y, z).")
