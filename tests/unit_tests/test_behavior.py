8b413423fa8781ab73796e22e48c3b8054984761
Cody Baker <cbaker9@nd.edu>
2022-04-14 14:16:46 -0400

    fix tests

--- tests/unit_tests/test_behavior.py
+++ tests/unit_tests/test_behavior.py
@@ -1,7 +1,7 @@
 from pynwb.behavior import SpatialSeries
 import numpy as np
 
-from nwbinspector.checks.behavior import check_spatial_series_dims
+from nwbinspector import InspectorMessage, Importance, check_spatial_series_dims
 
 
 def test_check_spatial_series_dims():
@@ -13,10 +13,13 @@ def test_check_spatial_series_dims():
         rate=3.0,
         reference_frame="reference_frame",
     )
-
-    assert (
-        check_spatial_series_dims(spatial_series).message == "SpatialSeries should have either 2 columns (x,"
-        "y) or 3 columns (x,y,z)."
+    assert check_spatial_series_dims(spatial_series) == InspectorMessage(
+        message="SpatialSeries should have 1 column (x), 2 columns (x, y), or 3 columns (x, y, z).",
+        importance=Importance.CRITICAL,
+        check_function_name="check_spatial_series_dims",
+        object_type="SpatialSeries",
+        object_name="SpatialSeries",
+        location="/",
     )
 
 
