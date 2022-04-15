9b9465988a59887839cddc5f3d4a096c6e5e47d9
bendichter <ben.dichter@gmail.com>
2022-04-14 21:58:01 -0400

    make subject species error message include current species name

--- tests/unit_tests/test_nwbfile_metadata.py
+++ tests/unit_tests/test_nwbfile_metadata.py
@@ -278,7 +278,7 @@ def test_check_subject_species_not_binomial():
     subject = Subject(subject_id="001", species="Human")
 
     assert check_subject_species_latin_binomial(subject) == InspectorMessage(
-        message="Species should be in latin binomial form, e.g. 'Mus musculus' and 'Homo sapiens'",
+        message="Subject species 'Human' should be in latin binomial form, e.g. 'Mus musculus' and 'Homo sapiens'",
         importance=Importance.BEST_PRACTICE_VIOLATION,
         check_function_name="check_subject_species_latin_binomial",
         object_type="Subject",
