ae33519f145535c7481e4e6d7d210b76341bec18
Cody Baker <cbaker9@nd.edu>
2022-04-26 13:30:01 -0400

    extend try/except to PyNWB validiation step

--- nwbinspector/nwbinspector.py
+++ nwbinspector/nwbinspector.py
@@ -454,30 +454,31 @@ def inspect_nwb(
     nwbfile_path = str(nwbfile_path)
     filterwarnings(action="ignore", message="No cached namespaces found in .*")
     filterwarnings(action="ignore", message="Ignoring cached namespace .*")
-    with pynwb.NWBHDF5IO(path=nwbfile_path, mode="r", load_namespaces=True, driver=driver) as io:
-        if skip_validate:
-            validation_errors = pynwb.validate(io=io)
-            if any(validation_errors):
-                for validation_error in validation_errors:
-                    yield InspectorMessage(
-                        message=validation_error.reason,
-                        importance=Importance.PYNWB_VALIDATION,
-                        check_function_name=validation_error.name,
-                        location=validation_error.location,
-                        file_path=nwbfile_path,
-                    )
-        try:
+    try:
+        with pynwb.NWBHDF5IO(path=nwbfile_path, mode="r", load_namespaces=True, driver=driver) as io:
+            if skip_validate:
+                validation_errors = pynwb.validate(io=io)
+                if any(validation_errors):
+                    for validation_error in validation_errors:
+                        yield InspectorMessage(
+                            message=validation_error.reason,
+                            importance=Importance.PYNWB_VALIDATION,
+                            check_function_name=validation_error.name,
+                            location=validation_error.location,
+                            file_path=nwbfile_path,
+                        )
+
             nwbfile = io.read()
-        except Exception as ex:
-            yield InspectorMessage(
-                message=traceback.format_exc(),
-                importance=Importance.ERROR,
-                check_function_name=f"{type(ex)}: {str(ex)}",
-                file_path=nwbfile_path,
-            )
-        for inspector_message in run_checks(nwbfile, checks=checks):
-            inspector_message.file_path = nwbfile_path
-            yield inspector_message
+            for inspector_message in run_checks(nwbfile, checks=checks):
+                inspector_message.file_path = nwbfile_path
+                yield inspector_message
+    except Exception as ex:
+        yield InspectorMessage(
+            message=traceback.format_exc(),
+            importance=Importance.ERROR,
+            check_function_name=f"{type(ex)}: {str(ex)}",
+            file_path=nwbfile_path,
+        )
 
 
 def run_checks(nwbfile: pynwb.NWBFile, checks: list):
