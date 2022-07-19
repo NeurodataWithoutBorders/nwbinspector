09b0474267d7209e7be8a927ce7b9a2fba650531
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2022-07-19 09:56:00 -0400

    Update nwbinspector/checks/tables.py
    Co-authored-by: Ben Dichter <ben.dichter@gmail.com>

--- nwbinspector/checks/tables.py
+++ nwbinspector/checks/tables.py
@@ -200,8 +200,7 @@ def check_col_not_nan(table: DynamicTable, nelems: Optional[int] = 200):
         if nelems is not None and not all(np.isnan(column[:nelems]).flatten()):
             continue
 
-        subindex_selection = safe_uniform_selection(length=column.shape[0], nelems=nelems)
-        if all(np.isnan(column[subindex_selection])):
+        if all(np.isnan(column[slice(0, None, np.ceil(length / nelems).astype(int) if nelems else None)])):
             yield InspectorMessage(
                 message=f"Column {column.name} has all NaN values. Consider removing it from the table."
             )
