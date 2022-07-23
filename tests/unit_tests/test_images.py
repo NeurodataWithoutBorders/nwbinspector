0b37307c1fc5390dbb7c25f6b43c42c059ab8700
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2022-07-22 21:09:30 -0400

    Update test_images.py

--- tests/unit_tests/test_images.py
+++ tests/unit_tests/test_images.py
@@ -7,14 +7,14 @@ from nwbinspector import InspectorMessage, Importance
 from nwbinspector.checks.images import check_order_of_images_unique, check_order_of_images_len
 
 try:
-    from pynwb.base import Images, ImageReferences
-    NOT_HAVE_PYNWB_GR_2_1_0 = True
-except:
-    NOT_HAVE_PYNWB_GR_2_1_0 = False
+    from pynwb.base import Images
+    HAVE_IMAGES = True
+except ImportError:
+    HAVE_IMAGES = False
 skip_reason = "You must have PyNWB>=v2.1.0 to run these tests!"
 
 
-@pytest.mark.skipif(NOT_HAVE_PYNWB_GR_2_1_0, reason=skip_reason)
+@pytest.mark.skipif(not HAVE_IMAGES, reason=skip_reason)
 def test_check_order_of_images_unique():
 
     imgs = [GrayscaleImage(name=f"image{i}", data=np.random.randn(10, 10)) for i in range(5)]
@@ -31,7 +31,7 @@ def test_check_order_of_images_unique():
     )
 
 
-@pytest.mark.skipif(NOT_HAVE_PYNWB_GR_2_1_0, reason=skip_reason)
+@pytest.mark.skipif(not HAVE_IMAGES, reason=skip_reason)
 def test_pass_check_order_of_images_unique():
 
     imgs = [GrayscaleImage(name=f"image{i}", data=np.random.randn(10, 10)) for i in range(5)]
@@ -41,7 +41,7 @@ def test_pass_check_order_of_images_unique():
     assert check_order_of_images_unique(images) is None
 
 
-@pytest.mark.skipif(NOT_HAVE_PYNWB_GR_2_1_0, reason=skip_reason)
+@pytest.mark.skipif(not HAVE_IMAGES, reason=skip_reason)
 def test_check_order_of_images_len():
 
     imgs = [GrayscaleImage(name=f"image{i}", data=np.random.randn(10, 10)) for i in range(5)]
@@ -58,7 +58,7 @@ def test_check_order_of_images_len():
     )
 
 
-@pytest.mark.skipif(NOT_HAVE_PYNWB_GR_2_1_0, reason=skip_reason)
+@pytest.mark.skipif(not HAVE_IMAGES, reason=skip_reason)
 def test_pass_check_order_of_images_len():
 
     imgs = [GrayscaleImage(name=f"image{i}", data=np.random.randn(10, 10)) for i in range(5)]
