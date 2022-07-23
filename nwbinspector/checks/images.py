a463a7dd90cefb8f57723629526040e4ccd65fac
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2022-07-22 21:08:13 -0400

    Update images.py

--- nwbinspector/checks/images.py
+++ nwbinspector/checks/images.py
@@ -4,21 +4,28 @@ from pynwb.base import Images
 
 from ..register_checks import register_check, Importance, InspectorMessage
 
-
-@register_check(importance=Importance.BEST_PRACTICE_VIOLATION, neurodata_type=Images)
-def check_order_of_images_unique(images: Images):
-    if images.order_of_images is None:
-        return
-    if not len(set(images.order_of_images)) == len(images.order_of_images):
-        return InspectorMessage(message="order_of_images should have unique values.")
+try:
+    from pynwb.base import Images
+    HAVE_IMAGES = True
+except ImportError:
+    HAVE_IMAGES = False
 
 
-@register_check(importance=Importance.BEST_PRACTICE_VIOLATION, neurodata_type=Images)
-def check_order_of_images_len(images: Images):
-    if images.order_of_images is None:
-        return
-    if not len(images.order_of_images) == len(images.images):
-        return InspectorMessage(
-            message=f"Length of order_of_images ({len(images.order_of_images)}) does not match the number of images ("
-            f"{len(images.images)})."
-        )
+if HAVE_IMAGES:
+    @register_check(importance=Importance.BEST_PRACTICE_VIOLATION, neurodata_type=Images)
+    def check_order_of_images_unique(images: Images):
+        if images.order_of_images is None:
+            return
+        if not len(set(images.order_of_images)) == len(images.order_of_images):
+            return InspectorMessage(message="order_of_images should have unique values.")
+
+
+    @register_check(importance=Importance.BEST_PRACTICE_VIOLATION, neurodata_type=Images)
+    def check_order_of_images_len(images: Images):
+        if images.order_of_images is None:
+            return
+        if not len(images.order_of_images) == len(images.images):
+            return InspectorMessage(
+                message=f"Length of order_of_images ({len(images.order_of_images)}) does not match the number of images ("
+                f"{len(images.images)})."
+            )
