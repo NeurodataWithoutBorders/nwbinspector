0fafb09e14cedf81705a1f443766f1540afade16
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2022-08-19 09:50:19 -0400

    Update general.py

--- nwbinspector/checks/general.py
+++ nwbinspector/checks/general.py
@@ -14,10 +14,9 @@ def check_name_slashes(obj):
 @register_check(importance=Importance.BEST_PRACTICE_SUGGESTION, neurodata_type=None)
 def check_description(obj):
     """Check if the description is a not missing or a placeholder."""
-    common_description_placeholders = ["no description", "no desc", "none"]
     if not hasattr(obj, "description"):
         return
     if obj.description is None or obj.description.strip(" ") == "":
         return InspectorMessage(message="Description is missing.")
-    if obj.description.lower().strip(".") in common_description_placeholders:
+    if obj.description.lower().strip(".") in COMMON_DESCRIPTION_PLACEHOLDERS:
         return InspectorMessage(message=f"Description ('{obj.description}') is a placeholder.")
