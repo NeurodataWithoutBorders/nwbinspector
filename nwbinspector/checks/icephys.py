071f167bf9686ef65f94375b2494c35fd81fd558
Cody Baker <cbaker9@nd.edu>
2022-09-20 10:02:26 -0400

    try swapping to hasattr

--- nwbinspector/checks/icephys.py
+++ nwbinspector/checks/icephys.py
@@ -6,5 +6,5 @@ from ..register_checks import register_check, Importance, InspectorMessage
 @register_check(importance=Importance.BEST_PRACTICE_VIOLATION, neurodata_type=IntracellularElectrode)
 def check_intracellular_electrode_cell_id_exists(intracellular_electrode: IntracellularElectrode):
     """Check if the IntracellularElectrode contains a cell_id."""
-    if getattr(intracellular_electrode, "cell_id", None) is None:
+    if getattr(intracellular_electrode, "cell_id", "") is None:  # Will only be None with PyNWB>=2.1.0
         return InspectorMessage(message="Please include a unique cell_id associated with this IntracellularElectrode.")
