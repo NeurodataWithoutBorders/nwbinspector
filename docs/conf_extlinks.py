2832795e003dfe73f9767d4931376469e7e604bb
Cody Baker <cbaker9@nd.edu>
2022-09-23 14:57:45 -0400

    fix ext links

--- docs/conf_extlinks.py
+++ docs/conf_extlinks.py
@@ -4,7 +4,7 @@ extlinks = {
     "matnwb-src": ("https://github.com/NeurodataWithoutBorders/matnwb/%s", "%s"),
     "nwb-overview": ("https://nwb-overview.readthedocs.io/en/latest/%s", "%s"),
     "conda-install": (
-        "https://docs.conda.io/projects/conda/en/latest/user-guide/install/index.html#regular-installation/%s",
+        "https://docs.conda.io/projects/conda/en/latest/user-guide/install/index.html#regular-installation%s",
         "%s",
     ),
     "nwbinspector-issues": ("https://github.com/NeurodataWithoutBorders/nwbinspector/issues/%s", "%s"),
@@ -13,10 +13,10 @@ extlinks = {
         "%s",
     ),
     "dandi-archive": ("https://dandiarchive.org/%s", "%s"),
-    "ros3-tutorial": ("https://pynwb.readthedocs.io/en/stable/tutorials/advanced_io/streaming.html/%s", "%s"),
+    "ros3-tutorial": ("https://pynwb.readthedocs.io/en/stable/tutorials/advanced_io/streaming.html%s", "%s"),
     "wikipedia": ("https://en.wikipedia.org/wiki/%s", "%s"),
     "allen-brain-map": ("https://%s.brain-map.org/", "%s"),
-    "uuid": ("https://docs.python.org/3/library/uuid.html/%s", "%s"),
+    "uuid": ("https://docs.python.org/3/library/uuid.html%s", "%s"),
     "orcid": ("https://orcid.org/%s", "%s"),
 }
 
