81a10e29ba36d458bf89c1b5d7d25856de3a8411
Cody Baker <cbaker9@nd.edu>
2022-11-09 18:43:05 -0500

    correct print path

--- src/nwbinspector/nwbinspector.py
+++ src/nwbinspector/nwbinspector.py
@@ -292,7 +292,7 @@ def inspect_all_cli(
     print_to_console(formatted_messages=formatted_messages)
     if report_file_path is not None:
         save_report(report_file_path=report_file_path, formatted_messages=formatted_messages, overwrite=overwrite)
-        print(f"{os.linesep*2}Report saved to {str(Path(json_file_path).absolute())}!{os.linesep}")
+        print(f"{os.linesep*2}Report saved to {str(Path(report_file_path).absolute())}!{os.linesep}")
 
 
 def inspect_all(
