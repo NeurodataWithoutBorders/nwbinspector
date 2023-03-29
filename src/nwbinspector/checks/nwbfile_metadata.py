1f495e26e6388dd7929d087de7484ec4cb6cc672
Ben Dichter <ben.dichter@gmail.com>
2023-03-29 14:50:00 -0400

    Update src/nwbinspector/checks/nwbfile_metadata.py
    Co-authored-by: Ryan Ly <rly@lbl.gov>

--- src/nwbinspector/checks/nwbfile_metadata.py
+++ src/nwbinspector/checks/nwbfile_metadata.py
@@ -200,9 +200,10 @@ def check_subject_sex(subject: Subject):
     """Check that the subject sex has been correctly specified."""
     if subject and not subject.sex:
         return InspectorMessage(message="Subject.sex is missing.")
-    if subject.species in ("Caenorhabditis elegans", "C. elegans") and subject.sex not in ("XO", "XX"):
-        return InspectorMessage(message="For C. elegans, Subject.sex should be 'XO' or 'XX'.")
-    if subject.sex not in ("M", "F", "O", "U"):
+    if subject.species in ("Caenorhabditis elegans", "C. elegans"):
+        if subject.sex not in ("XO", "XX"):
+            return InspectorMessage(message="For C. elegans, Subject.sex should be 'XO' or 'XX'.")
+    elif subject.sex not in ("M", "F", "O", "U"):
         return InspectorMessage(
             message="Subject.sex should be one of: 'M' (male), 'F' (female), 'O' (other), or 'U' (unknown)."
         )
