918808e70fcb0f45ce854793d5c2de5c50cdf082
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2024-04-28 13:39:03 -0400

    annotate global and try making relative to setup file

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ with open(root / "src" / "nwbinspector" / "version.py") as f:
     version = f.read()
 
 # Instantiate the testing configuration file from the base file `base_test_config.json`
-base_test_config = Path("./base_test_config.json")
-local_test_config = Path("./tests/testing_config.json")
-if not local_test_config.exists():
-    copy(src=base_test_config, dst=local_test_config)
+BASE_CONFIG_FILE_PATH = Path(__file__) / "base_test_config.json"
+TESTING_CONFIG_FILE_PATH = Path(__file__) / "tests" / "testing_config.json"
+if not TESTING_CONFIG_FILE_PATH.exists():
+    copy(src=BASE_CONFIG_FILE_PATH, dst=TESTING_CONFIG_FILE_PATH)
 
 setup(
     name="nwbinspector",
