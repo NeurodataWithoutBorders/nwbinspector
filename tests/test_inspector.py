9d5306b5be40af3175d40ab79ac274b9b79a0a7e
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-05-28 16:24:24 -0700

    added non-matching timestamps to sidecar file

--- tests/test_inspector.py
+++ tests/test_inspector.py
@@ -90,7 +90,7 @@ class TestInspector(TestCase):
             check_data_orientation,
             check_timestamps_match_first_dimension,
         ]
-        num_nwbfiles = 3
+        num_nwbfiles = 4
         nwbfiles = list()
         for j in range(num_nwbfiles):
             nwbfiles.append(make_minimal_nwbfile())
@@ -100,11 +100,11 @@ class TestInspector(TestCase):
         add_non_matching_timestamps_dimension(nwbfiles[0])
         add_simple_table(nwbfiles[0])
         add_regular_timestamps(nwbfiles[1])
+        add_non_matching_timestamps_dimension(nwbfiles[2])
         # Last file to be left without violations
 
         cls.nwbfile_paths = [str(cls.tempdir / f"testing{j}.nwb") for j in range(num_nwbfiles)]
-        nwbfiles.append(make_minimal_nwbfile())
-        cls.nwbfile_paths.append(str(cls.tempdir / "._testing0.nwb"))
+        cls.nwbfile_paths[2] = str(cls.tempdir / f"._testing2.nwb")
         for nwbfile_path, nwbfile in zip(cls.nwbfile_paths, nwbfiles):
             with NWBHDF5IO(path=nwbfile_path, mode="w") as io:
                 io.write(nwbfile)
