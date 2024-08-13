59e22fe83acddaae318b4044c1dab9e79033882e
Steph Prince <40640337+stephprince@users.noreply.github.com>
2024-08-13 12:55:52 -0700

    add stacklevel to soft deprecation warnings

--- src/nwbinspector/inspector_tools/__init__.py
+++ src/nwbinspector/inspector_tools/__init__.py
@@ -5,7 +5,7 @@ message = (
     "Please import the helper functions from the top-level package."
 )
 
-warnings.warn(message=message, category=DeprecationWarning)
+warnings.warn(message=message, category=DeprecationWarning, stacklevel=2)
 
 # Still keep imports functional with warning for soft deprecation cycle
 # TODO: remove after 9/15/2024
