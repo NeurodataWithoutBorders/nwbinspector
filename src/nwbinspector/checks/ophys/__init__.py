bc0b289a5ac52afc4affef7d0cad529783dc20a3
CodyCBakerPhD <codycbakerphd@gmail.com>
2024-08-14 15:05:53 -0400

    implement scope control over check submodule

---
+++ src/nwbinspector/checks/ophys/__init__.py
@@ -0,0 +1,13 @@
+import warnings
+
+message = (
+    "All submodules below the level 'nwbinspector.checks'  have been deprecated. "
+    "Please retrieve the specific check function(s) you wish to use from the `available_checks` registry "
+    "at the top-level or import directly from 'nwbinspector.checks'."
+)
+
+warnings.warn(message=message, category=DeprecationWarning, stacklevel=2)
+
+# Still keep imports functional with warning for soft deprecation cycle
+# TODO: remove after 9/15/2024
+from .._behavior import *
