bda40624b90222000e547f991f14e360ae4a4d9c
CodyCBakerPhD <codycbakerphd@gmail.com>
2024-08-19 15:27:23 -0400

    adjust imports for deprecated submodules

--- src/nwbinspector/checks/nwbfile_metadata/__init__.py
+++ src/nwbinspector/checks/nwbfile_metadata/__init__.py
@@ -10,4 +10,4 @@ warnings.warn(message=message, category=DeprecationWarning, stacklevel=2)
 
 # Still keep imports functional with warning for soft deprecation cycle
 # TODO: remove after 9/15/2024
-from .._behavior import *
+from .._nwbfile_metadata import *
