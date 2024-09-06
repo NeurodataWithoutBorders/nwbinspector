72041589f4d132d18f669d93d8dc41e3999fa5ab
CodyCBakerPhD <codycbakerphd@gmail.com>
2024-09-06 11:57:39 -0400

    swap to pyproject.toml

--- src/nwbinspector/__init__.py
+++ src/nwbinspector/__init__.py
@@ -1,3 +1,5 @@
+import importlib.metadata
+
 from ._version import __version__
 from ._registration import available_checks, register_check
 from ._types import Importance, Severity, InspectorMessage
@@ -22,6 +24,9 @@ from ._dandi_inspection import inspect_dandiset, inspect_dandi_file_path, inspec
 from .checks import *  # These need to be imported to trigger registration with 'available_checks', but are not exposed
 
 default_check_registry = {check.__name__: check for check in available_checks}
+
+# Still keeping the legacy magic version attribute requested by some users
+__version__ = importlib.metadata.version(distribution_name="nwbinspector")
 
 __all__ = [
     "available_checks",
