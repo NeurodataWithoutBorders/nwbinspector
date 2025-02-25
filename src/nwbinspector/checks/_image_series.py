8bc26f68157ab0059575b6d6209f97064f171d46
Steph Prince <40640337+stephprince@users.noreply.github.com>
2025-02-24 16:48:24 -0800

    add cross-platform abspath support

--- src/nwbinspector/checks/_image_series.py
+++ src/nwbinspector/checks/_image_series.py
@@ -1,7 +1,6 @@
 """Check functions specific to ImageSeries."""
 
-import ntpath
-from pathlib import Path
+from pathlib import Path, PurePosixPath, PureWindowsPath
 from typing import Iterable, Optional
 
 from pynwb.image import ImageSeries
@@ -45,7 +44,7 @@ def check_image_series_external_file_relative(image_series: ImageSeries) -> Opti
         return None
     for file_path in image_series.external_file:
         file_path = file_path.decode() if isinstance(file_path, bytes) else file_path
-        if ntpath.isabs(file_path):  # ntpath required for cross-platform detection
+        if PureWindowsPath(file_path).is_absolute() or PurePosixPath(file_path).is_absolute():
             yield InspectorMessage(
                 message=(
                     f"The external file '{file_path}' is not a relative path. "
